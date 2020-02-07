01707de773f493caa83ba82abf41a597dcee915c
Abel Kalum <chumbabel@gmail.com>
2020-02-07 01:15:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
   
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-  extend Findable
+  extend Findable::ClassMethods
   include Paramable
   attr_accessor :name
   attr_reader :songs
